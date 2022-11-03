fef11775f73d00654b9ba342268c9c73ed84563e
Raphael Son <raphaelson2000@yahoo.com>
2022-11-03 00:23:31 -0400

    Reformat calculator.js to use calculator object
    Previously used arrays, code cleaner at present

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const display = document.querySelector('.display');
+/*const display = document.querySelector('.display');
 display.textContent = "0";
 var currentOperator = "";
 const calculArray = [];
@@ -19,7 +19,7 @@ btns.forEach((btn) => {
                 break;
             
             case "operator":
-                if (calculArray.length > 1) { //a previous calculation is in the queue
+                if (calculArray.length > 1) { //a previous calc is in the queue
                     calculArray.push(display.textContent);
                     let newNumber = operate(calculArray[1], parseFloat(calculArray[0]), parseFloat(calculArray[2]));
                     calculArray.pop();
@@ -30,7 +30,7 @@ btns.forEach((btn) => {
                 else if (calculArray.length == 1) { //a number is in the queue, ie. "=" has just been clicked
                     calculArray.push(this.textContent);
                 }
-                else { //no previous calculations are in the queue
+                else { //no previous calcs are in the queue
                     calculArray.push(display.textContent);
                     calculArray.push(this.textContent);
                 }
@@ -45,7 +45,95 @@ btns.forEach((btn) => {
                 }
         }
     });
+});*/
+
+const calculatorScreen = document.querySelector('.display');
+const equationScreen = document.querySelector('.equation');
+const clearButton = document.querySelector('.clear');
+const equalsButton = document.querySelector('.equals');
+const backSpace = document.querySelector('.backspace');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+var calc = {
+    firstOperand: null, 
+    operator: null, 
+    secondOperand: null, 
+}
+
+numberButtons.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+        appendNumber(this.textContent);
+    });
 });
+
+operatorButtons.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+        if (calc.operator && calc.secondOperand) {
+            calc.firstOperand = calculate();
+            calc.secondOperand = null;    
+            calculatorScreen.textContent = calc.firstOperand;
+        }
+        calc.operator = this.textContent;
+        equationScreen.textContent = calc.firstOperand + " " + calc.operator + " ";
+    });
+});
+
+clearButton.addEventListener('click', function(e) {
+    for (data in calc) {
+        calc[data] = null;
+    }
+    calculatorScreen.textContent = "0";
+    equationScreen.textContent = "";
+});
+
+equalsButton.addEventListener('click', function(e) {
+    if (calc.operator && calc.secondOperand) {
+        calc.firstOperand = calculate();
+        calculatorScreen.textContent = calc.firstOperand;
+        equationScreen.textContent = calc.firstOperand;
+        calc.operator = null;
+        calc.secondOperand = null;
+    }
+});
+
+backSpace.addEventListener('click', function(e) {
+    if (calc.operator && calc.secondOperand) {
+        calc.secondOperand = null;
+        calculatorScreen.textContent = "";
+        equationScreen.textContent = calc.firstOperand + " " + calc.operator;
+    }
+    else if (calc.firstOperand) {
+        calc.firstOperand = null;
+        calc.operator = null;
+        equationScreen.textContent = "";
+    }
+    calculatorScreen.textContent = "";
+});
+
+function appendNumber(num) {
+    if (!calc.operator && !calc.secondOperand) {
+        calc.firstOperand == null ? calc.firstOperand = num : calc.firstOperand += num;
+        calculatorScreen.textContent = calc.firstOperand;
+        equationScreen.textContent = calc.firstOperand;
+    }
+    else if (calc.firstOperand && calc.operator) {
+        calc.secondOperand == null ? calc.secondOperand = num : calc.secondOperand += num;
+        calculatorScreen.textContent = calc.secondOperand;
+        equationScreen.textContent = calc.firstOperand + " " + calc.operator + " " + calc.secondOperand;
+    }
+}
+
+function calculate() {
+    if (calc.operator == "÷" && calc.secondOperand == "0") {
+        alert("You can't divide by 0! Please clear to prevent any errors.");
+        return "0";
+    }
+    else {
+        return Math.round(operate(calc.operator, parseFloat(calc.firstOperand), parseFloat(calc.secondOperand)).toString() * 10000000) / 10000000;
+    }
+    
+}
 
 function add(a,b) {
     return a+b;
